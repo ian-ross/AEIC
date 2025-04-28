42a6564f2f180695bc18c835e64ac26544f59915
Wyatt Giroux <girouxw@mit.edu>
2025-04-28 11:52:51 -0400

    commented out OAG_filter script reference in performance_model.py

--- src/AEIC/trajectories/legacy_trajectory.py
+++ src/AEIC/trajectories/legacy_trajectory.py
@@ -1,6 +1,7 @@
 import numpy as np
 from AEIC.performance_model import PerformanceModel
 from AEIC.trajectories.trajectory import Trajectory
+from utils.helpers import feet_to_meters
 
 class LegacyTrajectory(Trajectory):
     '''Model for determining flight trajectories using the legacy method
@@ -9,6 +10,13 @@ class LegacyTrajectory(Trajectory):
     def __init__(self, ac_performance:PerformanceModel):
         super().__init__(ac_performance)
         
+        # Climb defined as starting 3000' above airport
+        self.current_altitude = self.dep_lon_lat_alt[-1] + feet_to_meters(3000.0)
+        
+        
+    
+    
+    
     
     def climb(self):
         pass
--- src/AEIC/trajectories/trajectory.py
+++ src/AEIC/trajectories/trajectory.py
@@ -9,7 +9,8 @@ class Trajectory:
     def __init__(self, ac_performance:PerformanceModel, mission, optimize:bool):
         # Save A/C performance model and the mission to be flown
         # NOTE: Currently assume that `mission` comes in as a dictionary with the format of a single flight
-        # in `src/missions/sample_missions_10.json`
+        # in `src/missions/sample_missions_10.json`. We also assume that Load Factor for the flight will be
+        # included in the mission object.
         self.name = f'{mission["dep_airport"]}_{mission["arr_airport"]}_{mission["ac_code"]}'
         self.ac_performance = ac_performance
         
@@ -21,12 +22,11 @@ class Trajectory:
         self.end_time = mission["arr_datetime"]
         
         # Convert gc distance to meters
-        self.gc_distance = nautmiles_to_meters(mission["distance_nm"]) 
+        self.gc_distance = mission["distance_nm"] # FIXME: check to make sure this is changed to meters
     
         # Controls whether or not route optimization is performed
         # NOTE: This currently does nothing
-        self.optimize = optimize
-        
+        self.optimize = optimize        
     
     
     def calc_trajectory(self):
