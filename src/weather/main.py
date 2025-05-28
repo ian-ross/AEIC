0f39464f2c5f8bea5582bbc9cc22f9bdd2ab1cd9
prateekr9SF <119811887+prateekr9SF@users.noreply.github.com>
2025-05-28 16:35:24 -0400

    add traj w/ TAS

--- src/weather/main.py
+++ src/weather/main.py
@@ -4,36 +4,36 @@ import utils as util
 import trajectory as traj
 
 mission_path = "../missions/sample_missions_10.json"
+weather_data = "ERA5/sample.grib"
 
 # Load JSON data
 with open(mission_path, 'r') as file:
     missions = json.load(file)
     
 
-# Select a dummy mission for npw
+# Select a dummy mission for now
 first_mission = missions[0]
 
 
 # Get mission points based on mission def
-trajectory = traj.get_mission_points(first_mission)
+trajectory = traj.create_dummy_traj(first_mission)
 
 # Print the first few points
-for lon, lat, gs, alt in zip(trajectory["lons"][:5], trajectory["lats"][:5], trajectory["GS"][:5], trajectory["H"][:5]):
-    print(f"Lon: {lon:.2f}, Lat: {lat:.2f}, GS: {gs} kt, Alt: {alt} ft")
+print("\n")
+print("#--------------------------------SAMPLE TRAJECTORY (SNAPSHOT) -----------------------------#\n")
+for lon, lat, tas, alt in zip(trajectory["lons"][:5], trajectory["lats"][:5], trajectory["TAS"][:5], trajectory["H"][:5]):
+    print(f"Lon: {lon:.2f}, Lat: {lat:.2f}, TAS: {tas:.2f} kt, Alt: {alt:.2f} ft")
+print("#------------------------------------------------------------------------------------------#\n")
 
 
-#util.get_flight_track(trajectory)
-
-weather_data = "ERA5/sample.grib"
-
-#u, v, windmag = util.get_wind_at_points(trajectory, "ERA5/sample.grib")
 
 
-track, heading, drift, tas, u, v, wind_mag = util.get_tas(trajectory, "era5.grib")
 
-for i in range(5):
-    print(f"Pt {i}: Track={track[i]:.1f}°, Heading={heading[i]:.1f}°, Drift={drift[i]:+.1f}°, "
-          f"TAS={tas[i]:.1f} kt, U={u[i]:.1f}, V={v[i]:.1f}, Wind={wind_mag[i]:.1f} m/s")
+#track, heading, drift, tas, u, v, wind_mag = util.get_tas(trajectory, "era5.grib")
+
+#for i in range(5):
+#    print(f"Pt {i}: Track={track[i]:.1f}°, Heading={heading[i]:.1f}°, Drift={drift[i]:+.1f}°, "
+#          f"TAS={tas[i]:.1f} kt, U={u[i]:.1f}, V={v[i]:.1f}, Wind={wind_mag[i]:.1f} m/s")
 
 
 
--- src/weather/trajectory.py
+++ src/weather/trajectory.py
@@ -59,4 +59,74 @@ def get_mission_points(mission):
         "H": altitude_ft
     }
     
+def create_dummy_traj(mission):
+    """
+    Generates a discretized trapezoidal trajectory profile (altitude and speed)
+    along a geodesic path between departure and arrival locations.
+
+    Parameters
+    ----------
+    mission : dict
+        Dictionary with:
+            - 'dep_location': (longitude, latitude, altitude) of departure [degrees, degrees, feet]
+            - 'arr_location': (longitude, latitude, altitude) of arrival [degrees, degrees, feet]
+
+    Returns
+    -------
+    dict
+        Dictionary containing:
+            - 'lons': list of longitudes [degrees]
+            - 'lats': list of latitudes [degrees]
+            - 'TAS'  : list of True Air Speed [knots]
+            - 'H'   : list of altitudes [feet]
+    """
+    # Instantiate GGS84 ellipsoid for geodesic calculation
+    geod = Geod(ellps="WGS84")
+    
+    # Extract dept + Arrival lat-lon
+    lon_dep, lat_dep, _ = mission["dep_location"]
+    lon_arr, lat_arr, _ = mission["arr_location"]
+    
+    # Assume 100 points for trajectory discretization (demo only)
+    n_total = 100
+    points = geod.npts(lon_dep, lat_dep, lon_arr, lat_arr, n_total)
+    lons = [lon_dep] + [pt[0] for pt in points] + [lon_arr]
+    lats = [lat_dep] + [pt[1] for pt in points] + [lat_arr]
+    
+    # Define climb and descent length as 25 % of total trajectory
+    n_climb = n_descent = n_total // 4
+    n_cruise = len(lons) - n_climb - n_descent
+    
+    # Define end points for speed and altitude 
+    alt_start, alt_cruise = 1000, 35000  # feet
+    spd_start, spd_cruise = 140, 450 # knots
+    
+    # Define climb profile
+    climb_alt = np.linspace(alt_start, alt_cruise, n_climb)
+    climb_spd = np.linspace(spd_start, spd_cruise, n_climb)
+    
+    # Define cruise profile
+    cruise_alt = np.full(n_cruise, alt_cruise)
+    cruise_spd = np.full(n_cruise, spd_cruise)
+    
+    # Define descent profile
+    descent_alt = np.linspace(alt_cruise, alt_start, n_descent)
+    descent_spd = np.linspace(spd_cruise, spd_start, n_descent)
+    
+    
+
+    # Collect height and TAS
+    H = np.concatenate([climb_alt, cruise_alt, descent_alt])
+    TAS = np.concatenate([climb_spd, cruise_spd, descent_spd])
+    
+    H = H[:len(lons)]
+    TAS = TAS[:len(lons)]
+    
+    return {
+        "lons": lons,
+        "lats": lats,
+        "TAS": TAS,
+        "H": H
+    }
+    
     
