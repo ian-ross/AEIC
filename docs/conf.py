a1e9f22509f52738b8a83216f5a2e36686c69b38
Wyatt Giroux <girouxw@mit.edu>
2025-06-17 19:12:12 -0400

    added src/ to docs path

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
 project = 'AEIC'
 copyright = '2025, Wyatt Giroux, Prashanth Prakash, Prateek Ranjan, Aditeya Shukla, Raymond Speth'
