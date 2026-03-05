214ee479ccb9113db02a9a8dfa2d8085d315127e
Ian Ross <ian@skybluetrades.net>
2026-03-05 19:02:38 +0100

    Tests for performance model selection

--- tests/test_performance_model.py
+++ tests/test_performance_model.py
@@ -16,3 +16,19 @@ def test_performance_model_initialization():
     assert model.lto_performance.ICAO_UID == '01P11CM121'
 
     # TODO: Add tests for performance table.
+
+
+def test_performance_model_selection(performance_model_selector, sample_missions):
+    pms = [performance_model_selector(m).aircraft_name for m in sample_missions]
+    assert pms == [
+        'B738',
+        'B738',
+        'B738',
+        'B738',
+        'B738',
+        'A380',
+        'A380',
+        'B738',
+        'B738',
+        'A380',
+    ]
